c6af6387ee8976172649f229c9f0cee2f9d9374a
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-08-19 17:19:50 +0300

    CorsFilter class was corrected.

--- Kolibri-back-end/src/main/kotlin/com/ran/kolibri/component/CorsFilter.kt
+++ Kolibri-back-end/src/main/kotlin/com/ran/kolibri/component/CorsFilter.kt
@@ -14,6 +14,7 @@ class CorsFilter : Filter {
         response.setHeader("Access-Control-Allow-Origin", "*")
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
         response.setHeader("Access-Control-Max-Age", "86400")
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type")
         filterChain.doFilter(request, response)
     }
 
