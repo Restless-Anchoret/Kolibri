5e14d36b2b30fc9d46099b42683f6d6b93d9174b
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-08-19 23:04:31 +0300

    Page templates.html was implemented.

--- Kolibri-front-end/app/view-components/templates/templates.js
+++ Kolibri-front-end/app/view-components/templates/templates.js
@@ -3,7 +3,34 @@
 angular.module('kolibri')
     .component('templatesComponent', {
         templateUrl: 'view-components/templates/templates.html',
-        controller: function() {
+        controller: function($scope, $log, projectsService, NgTableParams) {
+            $scope.tableParams = new NgTableParams({}, {
+                getData: function(params) {
+                    return projectsService.getAllTemplateProjects(params.url()).then(function(data) {
+                        $log.debug('Template projects were successfully retrieved. Response:');
+                        $log.debug(data);
+                        params.total(data.totalElements);
+                        return data.content;
+                    });
+                }
+            });
 
+            $scope.projectOperations = [
+                { action: editProjectTemplate, name: 'Edit' },
+                { action: exportProjectTemplate, name: 'Export' },
+                { action: deleteProjectTemplate, name: 'Delete' }
+            ];
+
+            function editProjectTemplate(projectId) {
+                $log.debug('Editing project template. Id = ' + projectId);
+            }
+
+            function exportProjectTemplate(projectId) {
+                $log.debug('Exporting project template. Id = ' + projectId);
+            }
+
+            function deleteProjectTemplate(projectId) {
+                $log.debug('Deleting project template. Id = ' + projectId);
+            }
         }
     });
