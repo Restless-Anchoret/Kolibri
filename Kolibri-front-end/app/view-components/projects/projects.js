04f98710babae281fb784940ea1062e71563016e
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-08-19 22:34:21 +0300

    Page projects.html was implemented.

--- Kolibri-front-end/app/view-components/projects/projects.js
+++ Kolibri-front-end/app/view-components/projects/projects.js
@@ -3,7 +3,16 @@
 angular.module('kolibri')
     .component('projectsComponent', {
         templateUrl: 'view-components/projects/projects.html',
-        controller: function() {
-
+        controller: function($scope, $log, projectsService, NgTableParams) {
+            $scope.tableParams = new NgTableParams({}, {
+                getData: function(params) {
+                    return projectsService.getAllActiveProjects(params.url()).then(function(data) {
+                        $log.debug('Projects were successfully retrieved. Response:');
+                        $log.debug(data);
+                        params.total(data.totalElements);
+                        return data.content;
+                    });
+                }
+            });
         }
     });
