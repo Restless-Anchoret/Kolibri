e2ad5ec1b08d890f958fbad018af3480e6ef3364
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-08-20 16:12:45 +0300

    Page categories.html was implemented.

--- Kolibri-front-end/app/view-components/financial-project/categories/categories.js
+++ Kolibri-front-end/app/view-components/financial-project/categories/categories.js
@@ -3,7 +3,26 @@
 angular.module('kolibri')
     .component('financialProjectCategoriesComponent', {
         templateUrl: 'view-components/financial-project/categories/categories.html',
-        controller: function() {
+        controller: function($scope, $log, $stateParams, financialProjectsService, utilsService) {
+            var requestParams = {
+                projectId: $stateParams.projectId
+            };
 
+            $scope.tableParams = utilsService.createTableParams(
+                financialProjectsService.getFinancialProjectOperationCategories, requestParams
+            );
+
+            $scope.categoriesActions = [
+                { act: editCategory, name: 'Edit' },
+                { act: deleteCategory, name: 'Remove' }
+            ];
+
+            function editCategory(categoryId) {
+                $log.debug('Editing category. Id = ' + categoryId);
+            }
+
+            function deleteCategory(categoryId) {
+                $log.debug('Deleting category. Id = ' + categoryId);
+            }
         }
     });
