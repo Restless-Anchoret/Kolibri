4852f61aa2e184374aa7542d4d90a0d115f3f217
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-08-20 03:47:27 +0300

    Operations page was implemented partially.

--- Kolibri-front-end/app/view-components/financial-project/operations/operations.js
+++ Kolibri-front-end/app/view-components/financial-project/operations/operations.js
@@ -3,7 +3,36 @@
 angular.module('kolibri')
     .component('financialProjectOperationsComponent', {
         templateUrl: 'view-components/financial-project/operations/operations.html',
-        controller: function() {
+        controller: function($scope, $log, $stateParams, financialProjectsService, utilsService) {
+            var requestParams = {
+                projectId: $stateParams.projectId
+            };
 
+            $scope.tableParams = utilsService.createTableParams(
+                financialProjectsService.getFinancialProjectOperations, requestParams
+            );
+
+            $scope.operationsActions = [
+                { act: editOperation, name: 'Edit' },
+                { act: removeOperation, name: 'Remove' }
+            ];
+
+            $scope.isIncomeOperation = function(operation) {
+                return (operation.operationType === "income");
+            };
+            $scope.isExpendOperation = function(operation) {
+                return (operation.operationType === "expend");
+            };
+            $scope.isTransferOperation = function(operation) {
+                return (operation.operationType === "transfer");
+            };
+
+            function editOperation(operationId) {
+                $log.debug('Editing operation. Id = ' + operationId);
+            }
+
+            function removeOperation(operationId) {
+                $log.debug('Deleting operation. Id = ' + operationId);
+            }
         }
     });
