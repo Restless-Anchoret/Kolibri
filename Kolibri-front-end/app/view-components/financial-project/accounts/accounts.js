4995e9cde376aad97d03ba5e3b33adcc0d484226
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-08-20 16:00:05 +0300

    Page accounts.html was implemented.

--- Kolibri-front-end/app/view-components/financial-project/accounts/accounts.js
+++ Kolibri-front-end/app/view-components/financial-project/accounts/accounts.js
@@ -3,7 +3,26 @@
 angular.module('kolibri')
     .component('financialProjectAccountsComponent', {
         templateUrl: 'view-components/financial-project/accounts/accounts.html',
-        controller: function() {
+        controller: function($scope, $log, $stateParams, financialProjectsService, utilsService) {
+            var requestParams = {
+                projectId: $stateParams.projectId
+            };
 
+            $scope.tableParams = utilsService.createTableParams(
+                financialProjectsService.getFinancialProjectAccounts, requestParams
+            );
+
+            $scope.accountsActions = [
+                { act: editAccount, name: 'Edit' },
+                { act: removeAccount, name: 'Remove' }
+            ];
+
+            function editAccount(accountId) {
+                $log.debug('Editing account. Id = ' + accountId);
+            }
+
+            function removeAccount(accountId) {
+                $log.debug('Deleting account. Id = ' + accountId);
+            }
         }
     });
