38a6966f1e277919b57ff300f21343e7297ab85d
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-11-03 22:58:07 +0300

    ProjectProtectedAdvice class was implemented, new annotations were added.

---
+++ Kolibri-back-end/src/main/kotlin/com/ran/kolibri/security/ProjectProtectedAdvice.kt
@@ -0,0 +1,49 @@
+package com.ran.kolibri.security
+
+import com.ran.kolibri.exception.ForbiddenException
+import com.ran.kolibri.repository.project.ProjectRepository
+import com.ran.kolibri.security.annotation.ProjectId
+import com.ran.kolibri.service.UserService
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.reflect.MethodSignature
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Aspect
+@Component
+class ProjectProtectedAdvice {
+
+    @Autowired
+    lateinit var userService: UserService
+    @Autowired
+    lateinit var projectRepository: ProjectRepository
+
+    @Around("@annotation(com.ran.kolibri.security.annotation.ProjectProtected)")
+    @Transactional
+    fun aroundAdvice(joinPoint: ProceedingJoinPoint): Any {
+        val method = (joinPoint.signature as MethodSignature).method
+        var parameterIndex = -1
+        method.parameters.forEachIndexed { index, parameter ->
+            if (parameter.isAnnotationPresent(ProjectId::class.java)) {
+                parameterIndex = index
+            }
+        }
+
+        if (parameterIndex >= 0) {
+            val projectId = joinPoint.args[parameterIndex] as Long
+            val authenticatedUserId = userService.getAuthenticatedUserData().id
+
+            val project = projectRepository.findOne(projectId)
+            if (project != null && project.owner?.id != authenticatedUserId &&
+                    project.usersWithAccess.none { it.id == authenticatedUserId }) {
+                throw ForbiddenException("Current User has not access to this")
+            }
+        }
+
+        return joinPoint.proceed()
+    }
+
+}
